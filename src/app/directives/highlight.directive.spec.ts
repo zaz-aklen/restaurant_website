f49e1e5b816611378a5eba2535c96c433fdda4e6
lightning <nvsiroptics8@gamil.com>
2020-07-10 03:35:27 +0530

    menu component testing

--- src/app/directives/highlight.directive.spec.ts
+++ src/app/directives/highlight.directive.spec.ts
@@ -1,8 +1,11 @@
 import { HighlightDirective } from './highlight.directive';
+import { Directive, ElementRef, Renderer2, HostListener  } from '@angular/core';
 
 describe('HighlightDirective', () => {
   it('should create an instance', () => {
-    const directive = new HighlightDirective();
+    let el: ElementRef;
+    let renderer: Renderer2;
+    const directive = new HighlightDirective(el,renderer);
     expect(directive).toBeTruthy();
   });
 });
